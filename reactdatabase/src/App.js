752ab351f314efc36e2aa5ec6c96d56e0ade4c34
omar saade <omarsaadeh11@gmail.com>
2022-07-28 23:23:02 +0300

    react

--- reactdatabase/src/App.js
+++ reactdatabase/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
@@ -8,7 +8,10 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+
+
+
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
@@ -35,7 +38,16 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  }, []);
+
+
+
+  useEffect(() => {
+    fetchMoviesHandler();
+    //iza mnhet el function ka dependency it will produce an infinte loop , so to it  resolve we 
+    //use usecallback to fix the function 
+  }, [fetchMoviesHandler])
+
 
   let content = <p>Found no movies.</p>;
   if (movies.length > 0) { content = <MoviesList movies={movies} /> }
