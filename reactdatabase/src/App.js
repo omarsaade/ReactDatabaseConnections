155b3b1844a265fb174d0db15695b528c3905378
omar saade <omarsaadeh11@gmail.com>
2022-07-28 15:08:16 +0300

    react

--- reactdatabase/src/App.js
+++ reactdatabase/src/App.js
@@ -13,10 +13,13 @@ function App() {
     fetch('https://swapi.dev/api/films/')
       .then((response) => response.json())
       //.json() bta3mul translate lal json la javscript object
+      // response.json() hye promise reje3et 
       //array of object [{},{},{}]
       .then(data => {
         const transformedMovies = data.results.map(movieData => (
           //return la array of objects , li2an hye bta3mul retun la 6 object btheton bi array
+          //nehna ken fina nheta 3ade setMovies(data.results)
+          //bas li2an mabadna ngayer shi bel Movies.js text , fa 3melna i3adet tasmye men hon degre
           {
             id: movieData.episode_id,
             title: movieData.title,
