7d4e6f62b04c9858ea96cd84c016b5db524c2f07
omar saade <omarsaadeh11@gmail.com>
2022-07-28 16:45:27 +0300

    react

--- reactdatabase/src/App.js
+++ reactdatabase/src/App.js
@@ -6,33 +6,27 @@ import './App.css';
 function App() {
 
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   // fetch("https:spacex.xom").then((respone) => response.json()).then((data) => console.log(data))
+  async function fetchMoviesHandler() {
+    setIsLoading(true);
+    const response = await fetch('https://swapi.dev/api/films/');
+    const data = await response.json();
+    const transformedMovies = data.results.map(movieData => (
 
-  function fetchMoviesHandler() {
-    fetch('https://swapi.dev/api/films/')
-      .then((response) => response.json())
-      //.json() bta3mul translate lal json la javscript object
-      // response.json() hye promise reje3et 
-      //array of object [{},{},{}]
-      .then(data => {
-        const transformedMovies = data.results.map(movieData => (
-          //return la array of objects , li2an hye bta3mul retun la 6 object btheton bi array
-          //nehna ken fina nheta 3ade setMovies(data.results)
-          //bas li2an mabadna ngayer shi bel Movies.js text , fa 3melna i3adet tasmye men hon degre
-          {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date
-          }
+      {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date
+      }
 
+    ));
+    // console.log(transformedMovies); (6)[{… }, {… }, {… }, {… }, {… }, {… }]
 
-        ));
-        // console.log(transformedMovies); (6)[{… }, {… }, {… }, {… }, {… }, {… }]
-
-        setMovies(transformedMovies);
-      });
+    setMovies(transformedMovies);
+    setIsLoading(false);
   }
 
 
@@ -43,7 +37,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
@@ -51,5 +47,32 @@ function App() {
 
 export default App;
 
+
+
+// function fetchMoviesHandler() {
+//   fetch('https://swapi.dev/api/films/')
+//     .then((response) => response.json())
+//     //.json() bta3mul translate lal json la javscript object
+//     // response.json() hye promise reje3et
+//     //array of object [{},{},{}]
+//     //()=>{} //arrow function
+//     .then((data) => {
+//       const transformedMovies = data.results.map(movieData => (
+//         //return la array of objects , li2an hye bta3mul retun la 6 object btheton bi array
+//         //nehna ken fina nheta 3ade setMovies(data.results)
+//         //bas li2an mabadna ngayer shi bel Movies.js text , fa 3melna i3adet tasmye men hon degre
+//         {
+//           id: movieData.episode_id,
+//           title: movieData.title,
+//           openingText: movieData.opening_crawl,
+//           releaseDate: movieData.release_date
+//         }
+
+//       ));
+//       // console.log(transformedMovies); (6)[{… }, {… }, {… }, {… }, {… }, {… }]
+
+//       setMovies(transformedMovies);
+//     });
+// }
 
 
